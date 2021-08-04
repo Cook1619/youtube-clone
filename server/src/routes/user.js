2730de688c6d866e650ae5ba2114027a7bafe8b2
Mathew Cook <cook6348@gmail.com>
2021-08-04 07:28:01 -0500

    setting up liked videos endpoint

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,14 +1,51 @@
 import express from "express";
+import { protect } from "../middleware/authorization";
+import { PrismaClient } from "@prisma/client";
+import { getVideoViews } from "../controllers/video";
+
+const prisma = new PrismaClient();
 
 function getUserRoutes() {
   const router = express.Router();
 
+  router.get("/liked-videos", protect, getLikedVideos);
+
   return router;
 }
 
-async function getLikedVideos(req, res, next) {}
+async function getLikedVideos(req, res) {
+  const videoLikes = await prisma.videoLike.findMany({
+    where: {
+      userId: req.user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-async function getHistory(req, res, next) {}
+  const videoIds = videoLikes.map(videoLike => videoLike.videoId);
+
+  let videos = await prisma.video.findMany({
+    where: {
+      id: {
+        in: videoIds,
+      },
+    },
+    include: {
+      user: true,
+    },
+  });
+  if (videos.length) {
+    return res.status(200).json({ videos });
+  }
+  videos = await getVideoViews(videos);
+
+  return res.status(200).json({ videos });
+}
+
+async function getHistory(req, res) {
+  await getVideos(prisma.view, req, res);
+}
 
 async function toggleSubscribe(req, res, next) {}
 
