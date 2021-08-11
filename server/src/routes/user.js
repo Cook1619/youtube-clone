5cc01f2fcfc6da3f42ee5e879b1d92f42273303a
Mathew Cook <cook6348@gmail.com>
2021-08-11 07:11:03 -0500

    Adding user route to toggle subscribe

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -10,6 +10,7 @@ function getUserRoutes() {
 
   router.get("/liked-videos", protect, getLikedVideos);
   router.get("/history", protect, getHistory);
+  router.get("/:userId/toggle-subscribe", protect, toggleSubscribe);
 
   return router;
 }
@@ -53,7 +54,56 @@ async function getVideos(model, req, res) {
   return res.status(200).json({ videos });
 }
 
-async function toggleSubscribe(req, res, next) {}
+async function toggleSubscribe(req, res, next) {
+  if (req.user.id === req.params.userId) {
+    next({
+      message: "You cannot subscribe to your own channel",
+      statusCode: 400,
+    });
+  }
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.params.userId,
+    },
+  });
+  if (!user) {
+    next({
+      message: `No user found with id ${req.params.userId}`,
+      statusCode: 404,
+    });
+  }
+  const isSubscribed = await prisma.subscription.findFirst({
+    where: {
+      subscriberId: req.user.id,
+    },
+    subscribedToId: {
+      equals: req.params.userId,
+    },
+  });
+  if (isSubscribed) {
+    await prisma.subscription.delete({
+      where: {
+        id: isSubscribed.id,
+      },
+    });
+  } else {
+    await prisma.subscription.create({
+      data: {
+        subscriber: {
+          connect: {
+            id: req.user.id,
+          },
+        },
+        subscribedTo: {
+          connect: {
+            id: req.params.id,
+          },
+        },
+      },
+    });
+  }
+  res.status(200).json({});
+}
 
 async function getFeed(req, res) {}
 
