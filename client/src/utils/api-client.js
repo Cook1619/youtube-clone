68cde9b4a826d435ae5b48c9c14020e4dccac713
Mathew Cook <cook6348@gmail.com>
2021-08-30 16:17:09 -0500

    Hookig up API to upload video

--- client/src/utils/api-client.js
+++ client/src/utils/api-client.js
@@ -28,11 +28,17 @@ export async function signoutUser() {
 
 export async function updateUser() {}
 
-export async function addVideoView() {}
-
 export async function addComment() {}
 
-export async function addVideo() {}
+export async function addVideo(video) {
+  await client.post("/videos", video);
+  await queryCache.invalidateQueries("Channel");
+}
+
+export async function addVideoView(videoId) {
+  await client.get(`/videos/${videoId}/view`);
+  await queryCache.invalidateQueries("History");
+}
 
 export async function toggleSubscribeUser() {}
 
