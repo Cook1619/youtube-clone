c2647a1432ca7080c03bd5ccb0f2a54f0aee8ca6
Mathew Cook <cook6348@gmail.com>
2021-08-30 18:55:43 -0500

    setting up all endpoints for frontend

--- client/src/utils/api-client.js
+++ client/src/utils/api-client.js
@@ -26,7 +26,15 @@ export async function signoutUser() {
   window.location.pathname = "/";
 }
 
-export async function updateUser() {}
+export async function updateUser(user) {
+  await client.put("/users", user);
+  await queryCache.invalidateQueries("Channel");
+}
+
+export async function addVideoView(videoId) {
+  await client.get(`/videos/${videoId}/view`);
+  await queryCache.invalidateQueries("History");
+}
 
 export async function addComment({ video, comment }) {
   await client.post(`/videos/${video.id}/comments`, { text: comment });
@@ -38,18 +46,30 @@ export async function addVideo(video) {
   await queryCache.invalidateQueries("Channel");
 }
 
-export async function addVideoView(videoId) {
-  await client.get(`/videos/${videoId}/view`);
-  await queryCache.invalidateQueries("History");
+export async function toggleSubscribeUser(channelId) {
+  await client.get(`/users/${channelId}/toggle-subscribe`);
+  await queryCache.invalidateQueries("Channel");
+  await queryCache.invalidateQueries("Channels");
+  await queryCache.invalidateQueries("Subscriptions");
+  await queryCache.invalidateQueries("AuthProvider");
+  await queryCache.invalidateQueries("WatchVideo");
+  await queryCache.invalidateQueries("SearchResults");
 }
 
-export async function toggleSubscribeUser() {}
+export async function likeVideo(videoId) {
+  await client.get(`/videos/${videoId}/like`);
+  await queryCache.invalidateQueries(["WatchVideo", videoId]);
+}
 
-export async function likeVideo() {}
+export async function dislikeVideo(videoId) {
+  await client.get(`/videos/${videoId}/dislike`);
+  await queryCache.invalidateQueries(["WatchVideo", videoId]);
+}
 
-export async function dislikeVideo() {}
-
-export async function deleteVideo() {}
+export async function deleteVideo(videoId) {
+  await client.delete(`/videos/${videoId}`);
+  await queryCache.invalidateQueries("Channel");
+}
 
 export async function deleteComment(comment) {
   await client.delete(`/videos/${comment.videoId}/comments/${comment.id}`);
