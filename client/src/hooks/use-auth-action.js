f8201f2b80684719a591763a7f7ece64a80aacc3
Mathew Cook <cook6348@gmail.com>
2021-08-31 18:42:03 -0500

    Adding logic to pass auth around app

--- client/src/hooks/use-auth-action.js
+++ client/src/hooks/use-auth-action.js
@@ -1,1 +1,21 @@
-export default function useAuthAction() {}
+import { useGoogleLogin } from "react-google-login";
+import { useAuth } from "../context/auth-context";
+import { authenticate } from "../utils/api-client";
+
+export default function useAuthAction() {
+  const user = useAuth();
+  const { signIn } = useGoogleLogin({
+    onSuccess: authenticate,
+    clientId: "your-client-id",
+  });
+
+  function handleAuthAction(authAction, data) {
+    if (user) {
+      authAction(data);
+    } else {
+      signIn();
+    }
+  }
+
+  return handleAuthAction;
+}
